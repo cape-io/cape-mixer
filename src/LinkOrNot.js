76515df3c4410275c7c93e34dc6386731541e43c
Kai Curry <kai@cape.io>
2017-04-11 17:15:09 -0400

    cape-mixer

---
+++ src/LinkOrNot.js
@@ -0,0 +1,41 @@
+import React, { PropTypes } from 'react'
+import Link from 'redux-history-component'
+import { merge } from 'cape-redux'
+
+const styles = {
+  active: {
+    display: 'block',
+    position: 'relative',
+    height: '100%',
+    width: '100%',
+    overflow: 'hidden',
+    boxShadow: 'inset 0 0 12px rgba(66,67,47,.5)',
+    border: '3px solid rgba(66,67,47,1)',
+    backgroundPosition: 'top center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+  },
+}
+function getLinkStyles(bgImage) {
+  if (bgImage) return { backgroundImage: `url(${bgImage})` }
+  return {}
+}
+
+function LinkOrNot({ children, color, parent, bgImage }) {
+  const bgStyle = getLinkStyles(bgImage)
+  if (parent.id === color.id) return <span className="thisItem" style={merge(styles.active, bgStyle)}>{children}</span>
+  return (
+    <Link href={color.link} title={color.id} style={bgStyle} className="bg-cover">
+      {children}
+    </Link>
+  )
+}
+
+LinkOrNot.propTypes = {
+  children: PropTypes.node.isRequired,
+  color: PropTypes.object.isRequired,
+  parent: PropTypes.object.isRequired,
+  bgImage: PropTypes.string.isRequired,
+}
+
+export default LinkOrNot
